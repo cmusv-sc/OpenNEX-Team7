c8b0c4de1fd193b58ea1bd5ab4a958d86e9f812e
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-04-28 10:25:38 -0700

    Add some dummy services

---
+++ app/controllers/DummyServices.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.Arrays;
+
+/**
+ * Created by shbekti on 4/28/15.
+ */
+public class DummyServices extends Controller {
+
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result sort() {
+        JsonNode json = request().body().asJson();
+        String content = json.findPath("content").textValue();
+
+        if (content == null) {
+            return badRequest("Missing parameter [content]");
+        }
+
+        byte[] decodedBytes = Base64.decodeBase64(content.getBytes());
+        String decodedString = new String(decodedBytes);
+
+        String[] lines = decodedString.split("\n");
+        Arrays.sort(lines);
+
+        String joinedString = StringUtils.join(lines, "\n");
+
+        byte[] encodedBytes = Base64.encodeBase64(joinedString.getBytes());
+        String encodedString = new String(encodedBytes);
+
+        String jsonResponse = "{\"content\":\"" + encodedString + "\"" + "}";
+
+        return ok(jsonResponse);
+    }
+
+}
